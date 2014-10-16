17d51d08ff541108d83b7b5c7da75287598362c3
André Teves <andreteves@gmail.com>
2014-10-16 19:56:11 -0300

    more compact address form layout

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -1,7 +1,7 @@
 module Spree::AddressesHelper
   def address_field(form, method, id_prefix = "b", field_class = "", maxlength = nil, &handler)
     id_prefix = id_prefix == 'bill_address' ? 'b' : 's'
-    content_tag :p, :id => [id_prefix, method].join, :class => "field" do
+    content_tag :div, :id => [id_prefix, method].join, :class => "field" do
       if handler
         handler.call
       else
