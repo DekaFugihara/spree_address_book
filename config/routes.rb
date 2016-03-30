e9f95e33dbb274da40cea3fc8f1a0f250d19cb34
Denise Fugihara <contato@denika.com.br>
2016-03-30 19:22:04 -0300

    fix address removal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Spree::Core::Engine.routes.prepend do
-  resources :addresses
+  resources :addresses do
+  	get 'delete' => "addresses#destroy", :on => :member
+  end
+
   post 'ajax/address/retrieve_address_from_zipcode' => 'addresses#retrieve_address', :as => :retrieve_address_from_zipcode
 end
